729e5095b32bc5b499875d27b2c95eb9d326e50a
Benoît Ryder <benoit@ryder.fr>
2012-08-15 18:22:23 +0200

    Add CHECK log macros

--- log.h
+++ log.h
@@ -53,14 +53,22 @@ struct VoidMessage
 // Use boost::format formatting
 #define LOGF(severity, f)  LOG(severity) << boost::format(f)
 
+// Log message if condition is not fullfilled
+#define CHECK(severity, cond)  (cond) ? (void)0 : ::Log::VoidMessage() & LOG(severity)
+#define CHECKF(severity, cond, f)  (cond) ? (void)0 : ::Log::VoidMessage() & LOGF(severity, f)
+
+
 // Debug message
 #ifndef NDEBUG
 #define DLOG(severity)  LOG(severity)
 #define DLOGF(severity, f)  LOGF(severity, f)
+#define DCHECK(severity, cond)  CHECK(severity, cond)
+#define DCHECKF(severity, cond, f)  CHECKF(severity, conf, f)
 #else
-#define DLOG(severity)  true ? (void) 0 : ::Log::VoidMessage() & LOG(severity)
+#define DLOG(severity)  true ? (void)0 : ::Log::VoidMessage() & LOG(severity)
 #define DLOGF(severity, f)  true ? (void)0 : ::Log::VoidMessage() & LOGF(severity, f)
+#define DCHECK(severity, cond)  DLOG(severity)
+#define DCHECKF(severity, cond, f)  DLOGF(severity, f)
 #endif
-
 
 #endif
