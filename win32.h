62b2d7b80fb2806b49a2917aac462d22f1865eaa
Benoît Ryder <benoit@ryder.fr>
2012-12-29 16:12:25 +0100

    Add exception class for Win32 errors

---
+++ win32.h
@@ -0,0 +1,46 @@
+#ifndef WIN32_H__
+#define WIN32_H__
+
+#include <windows.h>
+#undef ERROR
+#include <exception>
+#include <string>
+
+
+/** @brief Exception class for Win32 errors
+ *
+ * @warning On mingw, GetLastError() must be called before the \e throw
+ * statement since it resets the error.
+ * That is also why there is no constructor with default error code.
+ */
+class win32_error: public std::exception
+{
+ public:
+  win32_error(DWORD code) noexcept:
+      code_(code), msg_(error_code_to_string(code)) {}
+  win32_error(DWORD code, const std::string& msg) noexcept:
+      code_(code), msg_(msg+": "+error_code_to_string(code)) {}
+
+  virtual ~win32_error() noexcept (true) {}
+  DWORD code() const noexcept { return code_; }
+  virtual const char* what() const noexcept { return msg_.c_str(); }
+
+ private:
+  static std::string error_code_to_string(DWORD code)
+  {
+    LPTSTR err = NULL;
+    if(!FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM, NULL, code, 0, (LPTSTR)&err, 0, NULL)) {
+      return "unknown";
+    } else {
+      std::string ret(err);
+      LocalFree(err);
+      return ret;
+    }
+  }
+
+  DWORD code_;
+  std::string msg_;
+};
+
+
+#endif
